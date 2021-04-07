099546546ad7998d12f7230395b798158a6ce751
Eldrick Millares <eldrick@stanford.edu>
2021-04-06 21:42:49 -0700

    Completes unregistered vec_mul

--- rtl/wrd/test_vec_mul.py
+++ rtl/wrd/test_vec_mul.py
@@ -2,24 +2,41 @@
 # SPDX-License-Identifier: CC0-1.0
 
 import random
+import numpy as np
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import FallingEdge
+from cocotb.binary import BinaryValue
+
+DUT_VECTOR_SIZE = 13
+
+
+def np2bv(int_arr):
+    """ Convert a 8b integer numpy array in cocotb BinaryValue """
+    int_list = int_arr.tolist()
+    binarized = [format(x & 0xFF, '08b') if x < 0 else format(x, '08b')
+                 for x in int_list]
+    bin_string = ''.join(binarized)
+    return BinaryValue(bin_string)
 
 
 @cocotb.test()
 async def test_vec_mul(dut):
     """ Test Vector Multiplier """
+    # Create a 10us period clock on port clk
+    clock = Clock(dut.clk_i, 10, units="us")
+    cocotb.fork(clock.start())
 
-    clock = Clock(dut.clk_i, 10, units="us")  # Create a 10us period clock on port clk
-    cocotb.fork(clock.start())  # Start the clock
-
-    await FallingEdge(dut.clk_i)  # Synchronize with the clock
+    # Synchronize with the clock
+    await FallingEdge(dut.clk_i)
     for i in range(10):
-        val = random.randint(0, 8)
+        val1 = np.random.randint(-128, 128, size=DUT_VECTOR_SIZE, dtype=np.int8)
+        val2 = np.random.randint(-128, 128, size=DUT_VECTOR_SIZE, dtype=np.int8)
 
-        dut.data1_i <= val
-        dut.data2_i <= val
+        for i in range(DUT_VECTOR_SIZE):
+            dut.data1_i <= np2bv(val1)
+            dut.data2_i <= np2bv(val2)
 
         dut.last1_i <= 0
         dut.last2_i <= 0
@@ -29,6 +46,10 @@ async def test_vec_mul(dut):
 
         dut.ready_i <= 1
 
+        expected = np2bv(val1 * val2)
         await FallingEdge(dut.clk_i)
-        assert dut.data_o.value == val * val,\
-               f"output was incorrect on the {i}th cycle"
+        for i in range(DUT_VECTOR_SIZE):
+            observed = dut.data_o.value
+            assert observed == expected,\
+                   "data1_i = %d, data2_i = %d, expected = %d, observed = %d" %\
+                   (val1[i], val2[i], expected, observed)
--- rtl/wrd/vec_mul.sv
+++ rtl/wrd/vec_mul.sv
@@ -1,34 +1,58 @@
 /*
  * Vector Multiplied
+ * TODO: Register outputs!
  */
 
 module vec_mul #(
-    parameter INPUT_BW = 8,
-    parameter OUTPUT_BW = 8,
+    parameter BW = 8,
     parameter VECTOR_SIZE = 13
 ) (
     input clk_i,
     input rstn_i,
 
-    input [VECTOR_SIZE * INPUT_BW - 1 : 0] data1_i,
+    input signed [(VECTOR_SIZE * BW) - 1 : 0] data1_i,
     input valid1_i,
     input last1_i,
     output ready1_o,
 
-    input [VECTOR_SIZE * INPUT_BW - 1 : 0] data2_i,
+    input signed [(VECTOR_SIZE * BW) - 1 : 0] data2_i,
     input valid2_i,
     input last2_i,
     output ready2_o,
 
-    output [VECTOR_SIZE * INPUT_BW - 1 : 0] data_o,
+    output signed [(VECTOR_SIZE * BW) - 1 : 0] data_o,
     output valid_o,
     output last_o,
     input  ready_i
 );
 
-    assign data_o = data1_i * data2_i;
-    assign valid_o = valid1_i & valid2_i;
-    assign last_o = last1_i | last2_i;
+    wire signed [BW - 1 : 0] data1_arr [VECTOR_SIZE - 1 : 0];
+    wire signed [BW - 1 : 0] data2_arr [VECTOR_SIZE - 1 : 0];
+    wire signed [BW - 1 : 0] out_arr   [VECTOR_SIZE - 1 : 0];
+
+    genvar i;
+
+    generate
+        for (i = 0; i < VECTOR_SIZE; i = i + 1) begin: unpack_inputs
+            assign data1_arr[i] = data1_i[(i + 1) * BW - 1 : i * BW];
+            assign data2_arr[i] = data2_i[(i + 1) * BW - 1 : i * BW];
+        end
+    endgenerate
+
+    generate
+        for (i = 0; i < VECTOR_SIZE; i = i + 1) begin: vector_multiply
+            assign out_arr[i] = data1_arr[i] * data2_arr[i];
+        end
+    endgenerate
+
+    generate
+        for (i = 0; i < VECTOR_SIZE; i = i + 1) begin: pack_output
+            assign data_o[(i + 1) * BW - 1 : i * BW] = out_arr[i];
+        end
+    endgenerate
+
+    assign valid_o  = valid1_i & valid2_i;
+    assign last_o   = last1_i | last2_i;
     assign ready1_o = ready_i;
     assign ready2_o = ready_i;
 
@@ -39,4 +63,5 @@ module vec_mul #(
       #1;
     end
     `endif
+
 endmodule
