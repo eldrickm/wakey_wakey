bf386ffd9229c56ab1ebe3c36c42129ed7e06557
Eldrick Millares <eldrick@stanford.edu>
2021-04-07 17:54:34 -0700

    Completes FIFO State Machine

--- rtl/wrd/conv1d/conv1d.sv
+++ rtl/wrd/conv1d/conv1d.sv
@@ -1,13 +1,12 @@
 /*
  * 1D Convolution
+ * TODO: Handle deassertion of valid
  */
-
-`define STATE_IDLE '
 
 module conv1d #(
     parameter BW = 8,
     parameter FRAME_SIZE = 50,
-    parameter VECTOR_SIZE = 13
+    parameter VECTOR_SIZE = 1
 ) (
     input                                      clk_i,
     input                                      rst_i_n,
@@ -24,23 +23,55 @@ module conv1d #(
 );
 
     localparam FILTER_SIZE = 3;
+    localparam NUM_FILTERS = 8;
+
     localparam VECTOR_BW = VECTOR_SIZE * BW;
+    localparam MAX_CYCLES = NUM_FILTERS * FRAME_SIZE;
+    localparam CYCLE_COUNT_BW = $clog2(MAX_CYCLES);
 
     genvar i;
 
     // === Start Conv1D FSM Logic ===
-    // TODO: What is size of state reg?
-    reg [4:0] conv1d_state;
-    always @(posedge clk) begin
+    // Define FIFO States
+    localparam FIFO_STATE_IDLE     = 2'd0,
+               FIFO_STATE_PRELOAD  = 2'd1,
+               FIFO_STATE_LOAD     = 2'd2,
+               FIFO_STATE_CYCLE    = 2'd3;
+    reg [1:0] fifo_state;
+
+    reg [CYCLE_COUNT_BW - 1 :0] cycle_counter;
+
+    wire last_i2 = last_i;
+
+    always @(posedge clk_i) begin
         if (!rst_i_n) begin
-            if (valid_i) begin
-                conv1d_state <= `STATE_LOAD;
-            end
+            cycle_counter <= 'd0;
+            fifo_state <= FIFO_STATE_IDLE;
         end else begin
-            conv1d_state <= `STATE_IDLE;
+            case (fifo_state)
+                FIFO_STATE_IDLE: begin
+                    cycle_counter <= 'd0;
+                    fifo_state <= (valid_i) ? FIFO_STATE_PRELOAD : FIFO_STATE_IDLE;
+                end
+                FIFO_STATE_PRELOAD: begin
+                    cycle_counter <= cycle_counter + 'd1;
+                    fifo_state <= (cycle_counter == FILTER_SIZE) ? FIFO_STATE_LOAD: FIFO_STATE_PRELOAD;
+                end
+                FIFO_STATE_LOAD: begin
+                    cycle_counter <= 'd0;
+                    fifo_state <= (last_i2) ? FIFO_STATE_CYCLE: FIFO_STATE_LOAD;
+                end
+                FIFO_STATE_CYCLE: begin
+                    cycle_counter <= cycle_counter + 'd1;
+                    fifo_state <= (cycle_counter < MAX_CYCLES - 1) ? FIFO_STATE_CYCLE : 
+                                  ((valid_i) ? FIFO_STATE_PRELOAD : FIFO_STATE_IDLE);
+                end
+                default: begin
+                    fifo_state <= FIFO_STATE_IDLE;
+                end
+            endcase
         end
     end
-
     // === End Conv1D FSM Logic ===
 
     // === Start Input FIFO Logic ===
@@ -51,6 +82,25 @@ module conv1d #(
     wire fifo_deq;
     wire fifo_not_full;
     wire fifo_not_empty;
+
+    // Needed to ensure we do not drop the data in the first cycle "valid"
+    // is asserted
+    reg [VECTOR_BW - 1: 0] din_q;
+    always @(posedge clk_i) begin
+        din_q <= data_i;
+    end
+
+    // Assign din to input if loading, otherwise feedback output
+    reg switch_inputs;
+    always @(posedge clk_i) begin
+        switch_inputs <= (fifo_state == FIFO_STATE_CYCLE);
+    end
+
+    assign fifo_din = switch_inputs ? fifo_out_sr[FILTER_SIZE - 1] : din_q;
+    assign fifo_enq = (fifo_state == FIFO_STATE_IDLE) ? 0 : 1;
+
+    assign fifo_deq = ((fifo_state == FIFO_STATE_CYCLE) | ((fifo_state == FIFO_STATE_PRELOAD) & (cycle_counter < FILTER_SIZE - 1)));
+    assign sr_enq = (switch_inputs | (fifo_state == FIFO_STATE_PRELOAD));
 
     fifo #(
         .DATA_WIDTH(VECTOR_BW),
@@ -73,16 +123,36 @@ module conv1d #(
     reg [VECTOR_BW - 1 : 0] fifo_out_sr [FILTER_SIZE - 1 : 0];
 
     always @(posedge clk_i) begin
-        fifo_out_sr[0] <= fifo_dout;
+        if (sr_enq) begin
+            fifo_out_sr[0] <= fifo_dout;
+        end
     end
 
     generate
         for (i = 1; i < FILTER_SIZE; i = i + 1) begin: create_fifo_out_sr
             always @(posedge clk_i) begin
-                fifo_out_sr[i] <= fifo_out_sr[i-1];
+                if (sr_enq) begin
+                    fifo_out_sr[i] <= fifo_out_sr[i-1];
+                end
             end
         end
     endgenerate
+
+    reg sr_valid_q, sr_valid_q2, sr_valid_q3;
+    always @(posedge clk_i) begin
+        sr_valid_q <= (fifo_state == FIFO_STATE_CYCLE);
+        sr_valid_q2 <= sr_valid_q;
+        sr_valid_q3 <= sr_valid_q2;
+    end
+
+    wire [VECTOR_BW - 1 : 0] sr0 = fifo_out_sr[0];
+    wire [VECTOR_BW - 1 : 0] sr1 = fifo_out_sr[1];
+    wire [VECTOR_BW - 1 : 0] sr2 = fifo_out_sr[2];
+
+    assign data_o = fifo_out_sr[FILTER_SIZE - 1];
+    assign valid_o = sr_valid_q;
+    assign ready_o = ready_i;
+    assign last_o = last_i;
     // === End Input FIFO Logic ===
 
     // Weight Bank
@@ -127,7 +197,8 @@ module conv1d #(
     `ifdef COCOTB_SIM
     initial begin
       $dumpfile ("conv1d.vcd");
-      $dumpvars (0, conv1d);
+      // $dumpvars (0, conv1d);
+      $dumpvars;
       #1;
     end
     `endif
---
+++ rtl/wrd/conv1d/test_conv1d.py
@@ -0,0 +1,62 @@
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+
+import numpy as np
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge, RisingEdge
+from cocotb.binary import BinaryValue
+
+DUT_VECTOR_SIZE = 13
+
+
+def np2bv(int_arr):
+    """ Convert a 8b integer numpy array in cocotb BinaryValue """
+    int_list = int_arr.tolist()
+    binarized = [format(x & 0xFF, '08b') if x < 0 else format(x, '08b')
+                 for x in int_list]
+    bin_string = ''.join(binarized)
+    return BinaryValue(bin_string)
+
+
+@cocotb.test()
+async def test_conv1d(dut):
+    """ Test Conv1D Module """
+    # Create a 10us period clock on port clk
+    clock = Clock(dut.clk_i, 10, units="us")
+    cocotb.fork(clock.start())
+
+    # Reset DUT
+    await FallingEdge(dut.clk_i)
+    dut.rst_i_n <= 0
+    dut.data_i <= 0
+    dut.valid_i <= 0
+    dut.last_i <= 0
+    dut.ready_i <= 0
+
+    for _ in range(20):
+        await FallingEdge(dut.clk_i)
+        dut.rst_i_n <= 1
+        dut.ready_i <= 1
+
+    # Load MFCC Data
+    for i in range(50):
+        await FallingEdge(dut.clk_i)
+        dut.data_i <= i
+        dut.valid_i <= 1
+        if i == 49:
+            dut.last_i <= 1
+
+    # Deassert End-Of-Packet Signals
+    await FallingEdge(dut.clk_i)
+    dut.last_i <= 0
+    dut.valid_i <= 0
+
+    await FallingEdge(dut.clk_i)
+    for i in range(50 * 8):
+        observed = dut.data_o.value
+        expected = i % 50
+        await FallingEdge(dut.clk_i)
+        assert observed == expected, "observed = %d, expected = %d," %\
+                                     (observed, expected)
