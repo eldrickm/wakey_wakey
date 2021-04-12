79fb607ee35ab916de9da229ea41633b9903af69
Eldrick Millares <eldrick@stanford.edu>
2021-04-12 00:45:21 -0700

    Refactors rtl lib, adds dffram

---
+++ rtl/lib/README.md
@@ -0,0 +1,6 @@
+Compiled list of OpenSource IP libraries / places to look:
+- [UW Basejump STL](https://github.com/bespoke-silicon-group/basejump_stl)
+- [TU Dresden Pile of Cores](https://github.com/VLSI-EDA/PoC)
+- [FOSSi LibreCores](https://www.librecores.org/) (would recommend over OpenCores)
+- [UCSC OpenRAM](https://github.com/VLSIDA/OpenRAM)
+- [efabless SKY130 OpenRAM Macros](https://github.com/efabless/sky130_sram_macros)
---
+++ rtl/lib/dffram/dffram.v
@@ -0,0 +1,37 @@
+// ============================================================================
+// Single Port RW DFF RAM
+// Design: Eldrick Millares
+// Verification: Matthew Pauly
+// Notes:
+// ============================================================================
+
+module dffram #(
+    parameter WIDTH = 32,
+    parameter DEPTH = 256
+) (
+    input                    clk_i,
+
+    input                    wr_en_i,
+    input                    en_i,
+
+    input  [ADDR_BW - 1 : 0] addr_i,
+    input  [WIDTH - 1 : 0]   data_i,
+    output [WIDTH - 1 : 0]   data_o
+);
+
+    localparam ADDR_BW = $clog2(DEPTH);
+
+    reg [WIDTH - 1 : 0] read_data;
+    reg [WIDTH - 1 : 0] mem [DEPTH - 1 : 0];
+
+    always @(posedge clk_i) begin
+        if (en_i) begin
+            read_data <= mem[addr_i];
+            if (wr_en_i) begin
+                mem[addr_i] <= data_i;
+            end
+        end
+    end
+
+    assign data_o = read_data;
+endmodule
