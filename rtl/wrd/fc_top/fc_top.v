c6aa2075f0e5871b876b2b4c74f9e45b4d977356
Eldrick Millares <eldrick@stanford.edu>
2021-04-22 13:31:37 -0700

    Adds non-functional fc_top

---
+++ rtl/wrd/fc_top/fc_top.v
@@ -0,0 +1,155 @@
+// ============================================================================
+// Fully Connected Layer
+// Design: Eldrick Millares
+// Verification: Matthew Pauly
+// Notes:
+// Assumes BIAS_BW > BW
+// TODO: Implement rd_data_o for reading memories
+// TODO: Calculate BW_O from NUM_CLASSES?
+// ============================================================================
+
+module fc_top #(
+    parameter BW_I        = 8,
+    parameter BIAS_BW     = BW_I * 2,
+    parameter BW_O        = BW_I * 3,
+    parameter FRAME_LEN   = 208,
+    parameter NUM_CLASSES = 3
+) (
+    input                               clk_i,
+    input                               rst_n_i,
+
+    // Input Features
+    input  signed [BW_I - 1 : 0]        data_i,
+    input                               valid_i,
+    input                               last_i,
+    output                              ready_o,
+
+    // Output Features
+    output signed [VECTOR_O_BW - 1 : 0] data_o,
+    output                              valid_o,
+    output                              last_o,
+    input                               ready_i,
+
+    // Memory Configuration Ports
+    input                               rd_en_i,
+    input                               wr_en_i,
+    input         [BANK_BW - 1 : 0]     rd_wr_bank_i,
+    input         [ADDR_BW - 1 : 0]     rd_wr_addr_i,
+    input  signed [BIAS_BW - 1 : 0]     wr_data_i,
+    output signed [BIAS_BW - 1 : 0]     rd_data_o
+);
+
+    genvar i;
+
+    // ========================================================================
+    // Local Parameters
+    // ========================================================================
+    // Bitwidth Definitions
+    localparam ADDR_BW   = $clog2(FRAME_LEN);
+    localparam VECTOR_I_BW = BW_O * NUM_CLASSES;
+    localparam VECTOR_BIAS_BW = BIAS_BW * NUM_CLASSES;
+    localparam VECTOR_O_BW = BW_O * NUM_CLASSES;
+
+    // Number of weight banks + bias bank per class
+    localparam BANK_BW          = $clog2(NUM_CLASSES * 2);
+    localparam FRAME_COUNTER_BW = $clog2(FRAME_LEN);
+    // ========================================================================
+
+    // ========================================================================
+    // Parameter Memory
+    // ========================================================================
+    wire [VECTOR_I_BW - 1 : 0] fc_mem_data_w;
+    wire [VECTOR_BIAS_BW - 1 : 0] fc_mem_data_b;
+    wire fc_mem_valid;
+    wire fc_mem_last;
+    wire fc_mem_ready;
+
+    fc_mem #(
+        .BW(BW_I),
+        .BIAS_BW(BIAS_BW),
+        .FRAME_LEN(FRAME_LEN),
+        .NUM_CLASSES(NUM_CLASSES)
+    ) fc_mem_inst (
+        .clk_i(clk_i),
+        .rst_n_i(rst_n_i),
+
+        .cycle_en_i(valid_i),
+
+        .rd_en_i(rd_en_i),
+        .wr_en_i(wr_en_i),
+        .rd_wr_bank_i(rd_wr_bank_i),
+        .rd_wr_addr_i(rd_wr_addr_i),
+        .wr_data_i(wr_data_i),
+        .rd_data_o(rd_data_o),
+
+        .data_w_o(fc_mem_data_w),
+        .data_b_o(fc_mem_data_b),
+        .valid_o(fc_mem_valid),
+        .last_o(fc_mem_last),
+        .ready_i(fc_mem_ready)
+    );
+
+    // ========================================================================
+    // MAC Array
+    // ========================================================================
+    // register stream input array to give fc_mem 1 cycle to ready outputs
+    reg  signed [BW_I - 1 : 0]        data_i_q;
+    reg                               valid_i_q;
+    reg                               last_i_q;
+    reg                               mac_ready0_q;
+
+    always @(posedge clk_i) begin
+        if (!rst_n_i) begin
+            data_i_q  <= 0;
+            valid_i_q <= 0;
+            mac_ready0_q <= 0;
+            last_i_q  <= 0;
+        end else begin
+            data_i_q  <= data_i;
+            valid_i_q <= valid_i;
+            mac_ready0_q <= mac_ready0;
+            last_i_q  <= last_i;
+        end
+    end
+
+    wire mac_ready0;
+
+    mac #(
+        .BW_I(BW_I),
+        .BW_O(BW_O),
+        .BW_BIAS(BIAS_BW),
+        .NUM_CLASSES(NUM_CLASSES)
+    ) mac_inst (
+        .clk_i(clk_i),
+        .rst_n_i(rst_n_i),
+
+        .data0_i(data_i_q),
+        .valid0_i(valid_i_q),
+        .last0_i(last_i_q),
+        .ready0_o(mac_ready0),
+
+        .data1_w_i(fc_mem_data_w),
+        .data1_b_i(fc_mem_data_b),
+        .valid1_i(fc_mem_valid),
+        .last1_i(fc_mem_last),
+        .ready1_o(fc_mem_ready),
+
+        .data_o(data_o),
+        .valid_o(valid_o),
+        .last_o(last_o),
+        .ready_i(ready_i)
+    );
+
+    // ========================================================================
+    // Simulation Only Waveform Dump (.vcd export)
+    // ========================================================================
+    `ifdef COCOTB_SIM
+    initial begin
+        $dumpfile ("wave.vcd");
+        $dumpvars (0, fc_top);
+        #1;
+    end
+    `endif
+    // ========================================================================
+
+endmodule
---
+++ rtl/wrd/fc_top/test_fc_top.py
@@ -0,0 +1,87 @@
+# This file is public domain, it can be freely copied without restrictions.
+# SPDX-License-Identifier: CC0-1.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import FallingEdge
+from cocotb.binary import BinaryValue
+
+@cocotb.test()
+async def test_fc_mem(dut):
+    # Create a 10us period clock on port clk
+    clock = Clock(dut.clk_i, 10, units="us")
+    cocotb.fork(clock.start())
+
+    # Reset system
+    await FallingEdge(dut.clk_i)
+    dut.data_i <= 0
+    dut.valid_i <= 0
+    dut.last_i <= 0
+    dut.ready_i <= 0
+    dut.rst_n_i <= 0
+    dut.wr_en_i <= 0
+    dut.rd_en_i <= 0
+    dut.rd_wr_bank_i <= 0
+    dut.rd_wr_addr_i <= 0
+    dut.wr_data_i <= 0
+
+    await FallingEdge(dut.clk_i)
+    dut.rst_n_i <= 1
+    dut.wr_en_i <= 0
+    dut.rd_en_i <= 0
+    dut.rd_wr_bank_i <= 0
+    dut.rd_wr_addr_i <= 0
+    dut.wr_data_i <= 0
+
+    await FallingEdge(dut.clk_i)
+
+    # Sequential Write Weights
+    for i in range(208 * 3):
+        dut.wr_en_i <= 1
+        dut.rd_en_i <= 0
+        dut.rd_wr_bank_i <= i // 208
+        dut.rd_wr_addr_i <= i % 208
+        dut.wr_data_i <= i % 208
+        await FallingEdge(dut.clk_i)
+
+    # Sequential Write Bias
+    for i in range(3):
+        dut.wr_en_i <= 1
+        dut.rd_en_i <= 0
+        dut.rd_wr_bank_i <= i + 3
+        dut.rd_wr_addr_i <= 0
+        dut.wr_data_i <= i
+        await FallingEdge(dut.clk_i)
+
+    # Sequential Read
+    for i in range(208 * 3):
+        dut.wr_en_i <= 0
+        dut.rd_en_i <= 1
+        dut.rd_wr_bank_i <= i // 208
+        dut.rd_wr_addr_i <= i % 208
+        dut.wr_data_i <= 0
+        await FallingEdge(dut.clk_i)
+
+    await FallingEdge(dut.clk_i)
+
+    # Check Cycling
+    for i in range(208):
+        dut.data_i <= i
+        dut.valid_i <= 1
+        if i == 207:
+            dut.last_i <= 1
+        else:
+            dut.last_i <= 0
+        dut.ready_i <= 1
+        dut.wr_en_i <= 0
+        dut.rd_en_i <= 0
+        dut.rd_wr_bank_i <= 0
+        dut.rd_wr_addr_i <= 0
+        dut.wr_data_i <= 0
+        await FallingEdge(dut.clk_i)
+
+    for i in range(50):
+        dut.valid_i <= 0
+        dut.last_i <= 0
+        await FallingEdge(dut.clk_i)
+
