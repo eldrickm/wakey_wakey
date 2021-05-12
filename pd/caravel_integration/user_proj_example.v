f3f8faf6f72a76c18d0be9c64075b3b7cd1c5083
Eldrick Millares <eldrick@stanford.edu>
2021-05-12 15:07:06 -0700

    Updates setup paths to remove .. indexing, adds caravel integration files

---
+++ pd/caravel_integration/user_proj_example.v
@@ -0,0 +1,107 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+module user_proj_example #(
+    parameter BITS = 32
+)(
+`ifdef USE_POWER_PINS
+    inout vdda1,	// User area 1 3.3V supply
+    inout vdda2,	// User area 2 3.3V supply
+    inout vssa1,	// User area 1 analog ground
+    inout vssa2,	// User area 2 analog ground
+    inout vccd1,	// User area 1 1.8V supply
+    inout vccd2,	// User area 2 1.8v supply
+    inout vssd1,	// User area 1 digital ground
+    inout vssd2,	// User area 2 digital ground
+`endif
+
+    // Wishbone Slave ports (WB MI A)
+    input wb_clk_i,
+    input wb_rst_i,
+    input wbs_stb_i,
+    input wbs_cyc_i,
+    input wbs_we_i,
+    input [3:0] wbs_sel_i,
+    input [31:0] wbs_dat_i,
+    input [31:0] wbs_adr_i,
+    output wbs_ack_o,
+    output [31:0] wbs_dat_o,
+
+    // Logic Analyzer Signals
+    input  [127:0] la_data_in,
+    output [127:0] la_data_out,
+    input  [127:0] la_oenb,
+
+    // IOs
+    input  [`MPRJ_IO_PADS-1:0] io_in,
+    output [`MPRJ_IO_PADS-1:0] io_out,
+    output [`MPRJ_IO_PADS-1:0] io_oeb,
+
+    // IRQ
+    output [2:0] irq
+);
+    // IO
+    assign io_oeb = {(`MPRJ_IO_PADS-1){wb_rst_i}};
+
+    // IRQ
+    assign irq = 3'b000;	// Unused
+
+    // LA
+    assign la_data_out = {{(127-BITS){1'b0}}, 32'b0};
+
+    wakey_wakey wakey_wakey_inst (
+        // clock and reset
+        .clk_i(wb_clk_i),
+        .rst_n_i(~wb_rst_i),
+
+        // wishbone slave ports (wb mi a)
+        .wbs_stb_i(wbs_stb_i),
+        .wbs_cyc_i(wbs_cyc_i),
+        .wbs_we_i(wbs_we_i),
+        .wbs_sel_i(wbs_sel_i),
+        .wbs_dat_i(wbs_dat_i),
+        .wbs_adr_i(wbs_adr_i),
+        .wbs_ack_o(wbs_ack_o),
+        .wbs_dat_o(wbs_dat_o),
+
+        // microphone inputs
+        // TODO
+
+        // wake output
+        .wake_o(io_out[0])
+    );
+endmodule
--- pd/setup.sh
+++ pd/setup.sh
@@ -1,19 +1,15 @@
-cd caravel_user_project
-
 # export caravel_root - should be caravel_user_project/caravel
-export CARAVEL_ROOT=$(pwd)/caravel
+export CARAVEL_ROOT=$(pwd)/caravel_user_project/caravel
 
 # export openlane tag, used to select version to build
 export OPENLANE_TAG=v0.15
-export OPENLANE_ROOT=$(pwd)/../openlane
+export OPENLANE_ROOT=$(pwd)/openlane
 
-export PDK_ROOT=$(pwd)/../pdk
+export PDK_ROOT=$(pwd)/pdk
 export PDK_PATH=$PDK_ROOT/sky130A
 
-export PRECHECK_ROOT=$(pwd)/../precheck
+export PRECHECK_ROOT=$(pwd)/precheck
 
-export GCC_PATH=$(pwd)/../riscv-tools-install/bin
+export GCC_PATH=$(pwd)/riscv-tools-install/bin
 
 module load magic
-
-cd ..
