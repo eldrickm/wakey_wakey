1e6332452dd663894facfb00c0d632116d8a6dd3
Matthew Pauly <mjpauly@mac.com>
2021-05-27 11:20:28 -0700

    break up critical path more

--- rtl/aco/power_spectrum/power_spectrum.v
+++ rtl/aco/power_spectrum/power_spectrum.v
@@ -30,20 +30,44 @@ module power_spectrum (
     localparam O_BW         = 32;
 
     // =========================================================================
+    // Register input to reduce long path length
+    // =========================================================================
+    reg signed [I_BW - 1 : 0] data_i_q;
+    reg valid_i_q;
+    reg last_i_q;
+    always @(posedge clk_i) begin
+        if (!rst_n_i | !en_i) begin
+            data_i_q <= 'd0;
+            valid_i_q <= 'd0;
+            last_i_q <= 'd0;
+        end else begin
+            if (valid_i) begin
+                data_i_q <= data_i;
+                valid_i_q <= valid_i;
+                last_i_q <= last_i;
+            end else begin
+                data_i_q <= 'd0;
+                valid_i_q <= 'd0;
+                last_i_q <= 'd0;
+            end
+        end
+    end
+
+    // =========================================================================
     // Unpacked Data
     // =========================================================================
-    wire signed [I_BW - 1 : 0] real_i = data_i[I_BW * 2 - 1 : I_BW];
-    wire signed [I_BW - 1 : 0] imag_i = data_i[I_BW - 1 : 0];
+    wire signed [I_BW - 1 : 0] real_i = data_i_q[I_BW * 2 - 1 : I_BW];
+    wire signed [I_BW - 1 : 0] imag_i = data_i_q[I_BW - 1 : 0];
 
     // =========================================================================
     // Result
     // =========================================================================
     wire [O_BW - 1 : 0] data = (real_i * real_i) + (imag_i * imag_i);
-    wire valid = valid_i;
-    wire last = last_i;
+    wire valid = valid_i_q;
+    wire last = last_i_q;
 
     // =========================================================================
-    // Register to reduce long path length
+    // Register output to reduce long path length
     // =========================================================================
     reg [O_BW - 1 : 0] data_q;
     reg valid_q;
@@ -54,9 +78,15 @@ module power_spectrum (
             valid_q <= 'd0;
             last_q <= 'd0;
         end else begin
-            data_q <= data;
-            valid_q <= valid;
-            last_q <= last;
+            if (valid) begin
+                data_q <= data;
+                valid_q <= valid;
+                last_q <= last;
+            end else begin
+                data_q <= 'd0;
+                valid_q <= 'd0;
+                last_q <= 'd0;
+            end
         end
     end
 
