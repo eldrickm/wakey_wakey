2fb6872b0bb8cebe3a0f22245463165b32b1bd3a
Eldrick Millares <eldrick@stanford.edu>
2021-05-30 11:29:38 -0700

    Disable PL_BASIC_PLACEMENT

--- pd/caravel_integration/openlane/user_proj_example/config.tcl
+++ pd/caravel_integration/openlane/user_proj_example/config.tcl
@@ -17,11 +17,13 @@ set script_dir [file dirname [file normalize [info script]]]
 
 set ::env(DESIGN_NAME) user_proj_example
 
+# Wakey Wakey Change: Add `design.v` to the VERILOG_FILES variable
 set ::env(VERILOG_FILES) "\
 	$script_dir/../../caravel/verilog/rtl/defines.v \
 	$script_dir/../../verilog/rtl/design.v \
 	$script_dir/../../verilog/rtl/user_proj_example.v"
 
+# Wakey Wakey Change: Only specify CLOCK_PORT, change frequency to 16 MHz
 # set ::env(CLOCK_PORT) ""
 # set ::env(CLOCK_NET) "counter.clk"
 # set ::env(CLOCK_PERIOD) "10"
@@ -30,10 +32,12 @@ set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(CLOCK_PERIOD) "62.5"
 
 set ::env(FP_SIZING) absolute
+# Wakey Wakey Change: Expand DIE_AREA
 # set ::env(DIE_AREA) "0 0 900 600"
 set ::env(DIE_AREA) "0 0 2920 3520"
 set ::env(DESIGN_IS_CORE) 0
 
+# Wakey Wakey Change: Remove analog power nets
 # set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
 # set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 set ::env(VDD_NETS) [list {vccd1} {vccd2}]
@@ -41,12 +45,14 @@ set ::env(GND_NETS) [list {vssd1} {vssd2}]
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
-set ::env(PL_BASIC_PLACEMENT) 1
+# Wakey Wakey Change: Disable PL_BASIC_PLACEMENT, increase PL_TARGET_DENSITY
+# set ::env(PL_BASIC_PLACEMENT) 1
 # set ::env(PL_TARGET_DENSITY) 0.05
+set ::env(PL_BASIC_PLACEMENT) 0
 set ::env(PL_TARGET_DENSITY) 0.50
 
 # If you're going to use multiple power domains, then keep this disabled.
 set ::env(RUN_CVC) 0
 
-# added to disable the [WARNING PDM-0030] messages in PDN
+# Wakey Wakey Change: Added to disable the [WARNING PDM-0030] messages in PDN
 set ::env(FP_PDN_CHECK_NODES) 0
