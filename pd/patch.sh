e3702a5795b0440b2d3161a88c3fbc8a93eb2f25
Eldrick Millares <eldrick@stanford.edu>
2021-05-30 14:30:15 -0700

    Updates macro.cfg for pipecleaner, reverts user_project_wrapper config.tcl

--- pd/patch.sh
+++ pd/patch.sh
@@ -15,3 +15,6 @@ cp caravel_integration/openlane/user_proj_example/config.tcl caravel_user_projec
 
 # pull in user_project_wrapper/config.tcl changes for openlane flow
 cp  caravel_integration/openlane/user_project_wrapper/config.tcl caravel_user_project/openlane/user_project_wrapper/config.tcl 
+
+# pull in user_project_wrapper/macros.cfg changes for openlane flow
+cp caravel_integration/openlane/user_project_wrapper/macro.cfg caravel_user_project/openlane/user_project_wrapper/macro.cfg 
