8bc9264c8d0de17cb41de988f3d082e75b147c55
Eldrick Millares <eldrick@stanford.edu>
2021-05-29 20:53:13 -0700

    Updates pd/caravel_integration structure, changes user_proj_example wiring

--- pd/sync.sh
+++ pd/sync.sh
@@ -4,5 +4,8 @@ cp -r caravel_user_project/verilog/rtl/ caravel_integration
 # pull in dv files
 cp -r caravel_user_project/verilog/dv/wakey_wakey_test/ caravel_integration
 
-# pull in config.tcl changes for openlane flow
-cp caravel_user_project/openlane/user_project_wrapper/config.tcl caravel_integration
+# pull in user_proj_example/config.tcl changes for openlane flow
+cp caravel_user_project/openlane/user_proj_example/config.tcl caravel_integration/openlane/user_proj_example
+
+# pull in user_project_wrapper/config.tcl changes for openlane flow
+cp caravel_user_project/openlane/user_project_wrapper/config.tcl caravel_integration/openlane/user_project_wrapper
