caf8af597ff0720f003b0edd758b3e1f681331b6
Eldrick Millares <eldrick@stanford.edu>
2021-06-17 21:03:25 -0700

    Updates run_extract fro caddyxtra

--- pd_proprietary/extraction/run_extract.sh
+++ pd_proprietary/extraction/run_extract.sh
@@ -1,6 +1,6 @@
 export design_name="user_project_wrapper"
-export PDKPATH=${PWD}/../../sky130A
+export PDKPATH=${PWD}/../../pd/pdk/sky130A
 
-cp ../.magicrc .
+# cp ../.magicrc .
 magic -noconsole -dnull extract.tcl | tee gds2spice.log
 mv user_project_wrapper.spice design_extracted.spice
